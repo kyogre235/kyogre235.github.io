b4b5950841f7a8f5428b5208ca8d65b1dd279889
Alejandro Jacome <jacomedelgadoa@ciencias.unam.mx>
2023-11-05 15:23:06 -0600

    otra plantilla

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,55 @@
-// nav menu style
-var nav = $("#navbarSupportedContent");
-var btn = $(".custom_menu-btn");
-btn.click
-btn.click(function (e) {
 
-    e.preventDefault();
-    nav.toggleClass("lg_nav-toggle");
-    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
-});
+  $(function () {
+    'use strict'
 
+    // MENU
+    $('.navbar .nav-link').on('click',function(){
+        $(".navbar-collapse").collapse('hide');
+    });
 
-function getCurrentYear() {
-    var d = new Date();
-    var currentYear = d.getFullYear()
+    $(window).on('scroll', function() {     
+                                
+        /*----------------------------------------------------*/
+        /*  Navigtion Menu Scroll
+        /*----------------------------------------------------*/    
+        
+        var b = $(window).scrollTop();
+        
+        if( b > 72 ){       
+            $(".navbar").addClass("scroll");
+        } else {
+            $(".navbar").removeClass("scroll");
+        }               
+    });
 
-    $("#displayDate").html(currentYear);
-}
+    // TESTIMONIALS CAROUSEL
+    $('#testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            900:{
+                items:2,
+            },
+            1200:{
+                items:3,
+                loop:false
+            }
+        }
+    })
 
-getCurrentYear();
+    // SMOOTHSCROLL
+    $(function() {
+      $('.navbar .nav-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });   
+     
+  });
